70dd02e72bc1f49e3a5fbc31e9a4325e75eaa6ee
Gretchen Harris <gretchenh77@yahoo.com>
2020-06-19 16:05:33 -0400

    finished

--- main.js
+++ main.js
@@ -8,23 +8,16 @@ const mainElement = document.createElement('main')
 bodyElement.append(mainElement)
 
 
-
-
-
-
-//https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Dr_Pepper_bottle.JPG/260px-Dr_Pepper_bottle.JPG
-//<img src="" alt="">
-//<a href=""></a>
 const imgElement = document.createElement('img')
 imgElement.className = 'image'
 imgElement.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Dr_Pepper_bottle.JPG/260px-Dr_Pepper_bottle.JPG'
 mainElement.append(imgElement)
 
  const anchorElement = document.createElement('a')
- anchorElement = 'link'
+ anchorElement.className = 'link'
  anchorElement.href = 'https://www.wikipedia.org/'
- mainElement.append(anchorElement)
  anchorElement.append('WIKIPEDIA')
+ mainElement.append(anchorElement)
 
 
 buttonElement.addEventListener('click', function () {
